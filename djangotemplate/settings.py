5b2fbcd91eba5dcafb181a6808d3a31e34aae139
James Byars <jamesbyars@g.harvard.edu>
2018-04-24 15:53:40 -0500

    Add login form

--- djangotemplate/settings.py
+++ djangotemplate/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'djangotemplate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./djangotemplate/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
